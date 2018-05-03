9901b20730c71e605957169d4d5875cc973b58bf
王丹 <wangdan@dycd.com>
2018-05-03 16:29:07 +0800

    webpack配置

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+/**
+ * Created by wangdan on 2018/5/3.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss'
+
+ReactDOM.render(
+	<div>
+		{/*<i className="fa fa-address-book"></i>*/}
+		<h1>hello world!</h1>
+	</div>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+/**
+ * Created by wangdan on 2018/5/3.
+ */
+const path = require('path');
+const HtmlWebpackPlugin= require('html-webpack-plugin');
+const ExtractTextPlugin= require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+	entry: './src/app.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: 'js/app.js'
+	},
+	module: {
+		rules: [
+			{
+				// js脚本文件配置
+				test: /\.jsx$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env','react']
+					}
+				}
+			},
+			{
+				// css文件配置
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader"
+				})
+			},
+			{
+				// sass配置
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: ["css-loader","sass-loader"]
+				})
+			},
+			{
+				// 图片配置
+				test: /\.(png|jpg|gif|jpeg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name :'resource/[name].[ext]'
+						}
+					}
+				]
+			},
+			// {
+			// 	test: /\.(png|jpg|gif|jpeg)$/,
+			// 	use: [
+			// 		{
+			// 			loader: 'file-loader',
+			// 			options: {
+			// 				limit: 8192,
+			// 				name :'resource/[name].[ext]'
+			// 			}
+			// 		}
+			// 	]
+			// },
+			{
+				// 字体图标配置
+				test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name :'resource/[name].[ext]'
+						}
+					}
+				]
+			},
+		]
+	},
+	plugins: [
+		// html配置
+		new HtmlWebpackPlugin({
+			template:'./src/index.html'
+		}),
+		// 独立css文件
+		new ExtractTextPlugin("css/[name].css"),
+		// 提出公共模块
+		new webpack.optimize.CommonsChunkPlugin({
+			name : 'common',
+			filename : 'js/base.js'
+		})
+	],
+	// webpack-dev-server的处理
+	devServer: {
+		// contentBase:'./dist',
+		port : 8888
+	}
+};
